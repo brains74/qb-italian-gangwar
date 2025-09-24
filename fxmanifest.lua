5985086622cc98da6b8bc4b6afa37d125c0f618f
brains74 <youshop@hotmail.it>
2025-09-24 19:35:38 +0200

    Initial fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+description 'QBCore Italian Gang War Script'
+
+author 'brains74'
+
+version '1.0.0'
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
+
+files {
+    'config.lua'
+}
+
+lua54 'yes'
